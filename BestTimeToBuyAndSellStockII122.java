3f681a4a55c7918c48a12efbb76cdc944dfe1b09
95275059 <1227837302@qq.com>
2021-05-08 13:32:49 +0800

    2021.05.08

---
+++ BestTimeToBuyAndSellStockII122.java
@@ -0,0 +1,39 @@
+
+public class BestTimeToBuyAndSellStockII122 {
+	public static int maxProfit(int[] prices) {
+		// 执行用时：2 ms, 在所有 Java 提交中击败了36.24%的用户
+		// 内存消耗：38.3 MB, 在所有 Java 提交中击败了46.77%的用户
+		int len = prices.length;
+		if (len <= 1) 
+			return 0;
+		int hold = -prices[0];
+		int nohold = 0;
+		for (int i=1; i<len; i++) {
+			hold = Math.max(hold, nohold - prices[i]);
+			nohold = Math.max(nohold, hold + prices[i]);
+		}
+		return nohold;
+	}
+	public static int maxProfit1(int[] prices) {
+		// 执行用时：1 ms, 在所有 Java 提交中击败了99.62%的用户
+		// 内存消耗：38.1 MB, 在所有 Java 提交中击败了82.37%的用户
+		int len = prices.length;
+		if (len <= 1)
+			return 0;
+		int profit = 0;
+		for (int i=0; i<len-1; i++)
+			profit += Math.max(prices[i+1] - prices[i], 0);
+		return profit;
+	}
+	public static void main(String[] arg) {
+		int[] prices = new int[6];
+		prices[0] = 7;
+		prices[1] = 1;
+		prices[2] = 5;
+		prices[3] = 3;
+		prices[4] = 6;
+		prices[5] = 4;
+		int profit = maxProfit(prices);
+		System.out.println(profit);
+	}
+}
